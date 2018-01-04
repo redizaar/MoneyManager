bdcb66dcdcd65c5856c78814d54b024cfa3fd1ec
redizaar <pwnpwn90@gmail.com>
2018-01-04 22:49:26 +0100

    Add files via upload

--- WpfApp1/WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("keyicon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("loginpanelbackground.jpg")]
 
 
