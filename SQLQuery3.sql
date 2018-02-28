80dc99f840995d532cbbd5afaf8788968d462aa6
redizaar <pwnpwn90@gmail.com>
2018-02-28 17:26:44 +0100

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROC insertStockDataNew3
+@name varchar(50),
+@price float,
+@date varchar(50),
+@time_id varchar(50)
+AS
+	INSERT INTO StockData.dbo.[Stock_WebData](Name,Price,Date,TIME_ID)
+	VALUES (@name,@price,@date,@time_id)
+
---
+++ SQLQuery6.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROC registrationQuery3
+@username varchar(50),
+@password varchar(50),
+@accountnumber varchar(50)
+AS
+	INSERT INTO LoginDB.dbo.[UserDatas](Username,Password,AccountNumber)
+	VALUES (@username,@password,@accountnumber)
